150dfccb2361f2d0ddbb592bc2e9ca2086879d71
ava fox <dev@computerfox.xyz>
2022-02-01 18:13:02 -0500

    split out logic into its own function
    fixed various bugs

--- gamma.lisp
+++ gamma.lisp
@@ -8,7 +8,7 @@
   "default output directory")
 (defvar *templates* (make-hash-table :test 'equal)
   "hash table containing all compiled templates keyed off of filename")
-(defvar *default-template* (cl-template:compile-template "<html><body>(@ content)</body></html>")
+(defvar *default-template* (cl-template:compile-template "<html><body><%= @ content %></body></html>")
   "a basic HTML template")
 (defvar *static-dir* nil
   "folder that holds static assets - set via config file")
@@ -33,6 +33,45 @@
      :arg-parse #'identity
      :meta-var "DIRECTORY"))
 
+(defun walk-directory-and-generate-html (path)
+  (uiop:with-current-directory ((pathify path))
+    (conf:load-config *config*)
+
+    (setf *static-dir* (conf:config :static-dir)
+          *output-dir* (pathify *output-dir*))
+  
+    ;; loads and compiles all of our tempaltes
+    (when (conf:config :template-dir)
+      (loop :for f :in (uiop:directory-files (conf:config :template-dir))
+            :do (setf (gethash (pathname-name f) *templates*)
+                      (cl-template:compile-template (str:from-file f)))))
+
+    ;; create our output directory
+    (ensure-directories-exist *output-dir*)
+
+    (let* ((dirs (list* "." (site-dirs "." :ignore (remove-if #'null
+                                                              (list* *output-dir*
+                                                                     *static-dir*
+                                                                     (conf:config :template-dir)
+                                                                     (conf:config :ignore-dirs))))))
+           (files (alexandria:flatten (loop :for dir :in dirs
+                                            :do (ensure-directories-exist (merge-pathnames (make-relative-path dir)
+                                                                                           *output-dir*))
+                                            :collect (uiop:directory-files dir "*.md")))))
+      (loop :for file :in files
+            :do (let* ((parsed-output (parse-markdown file))
+                       (output-path (merge-pathnames (change-extension (make-relative-path file) "html")
+                                                     *output-dir*))
+                       (templ-func (gethash (getf parsed-output :template) *templates* *default-template*)))
+                  (format t "~S" (getf parsed-output :template))
+                  (str:to-file output-path (funcall templ-func parsed-output)
+                               :if-exists :overwrite))))
+
+    ;; now that our site files are created, lets copy over our static
+    ;;  assets if they exist
+    (when (and *static-dir* (uiop:directory-exists-p *static-dir*))
+      (copy-directory *static-dir* *output-dir*))))
+
 (defun main ()
   (multiple-value-bind (options free-args) (opts:get-opts)
     (when (getf options :help)
@@ -41,47 +80,15 @@
       (opts:exit 0))
 
     (when (getf options :version)
-      (format t "gamma v~A~&" #.(asdf:component-version (asdf:find-system :gamma))))
+      (format t "gamma v~A~&" #.(asdf:component-version (asdf:find-system :gamma)))
+      (opts:exit 0))
 
-    (when (getf options :config)
-      (setf *config* (getf options :config)))
+    (when (getf options :config-file)
+      (setf *config* (getf options :config-file)))
+
     
     (when (getf options :output-dir)
       (setf *output-dir* (getf options :output-dir))))
   
-  (load-config *config*)
-
-  (setf *static-dir* (conf:config :static-dir)
-        *output-dir* (pathify *output-dir*))
-  
-  ;; loads and compiles all of our tempaltes
-  (when (config :template-dir)
-    (loop :for f :in (uiop:directory-files (config :template-dir))
-          :do (setf (gethash (pathname-name f) *templates*)
-                    (cl-template:compile-template (str:from-file f)))))
-
-  ;; create our output directory
-  (ensure-directories-exist *output-dir*)
-
-  (let* ((dirs (list* "." (site-dirs "." :ignore (list* *output-dir*
-                                                        *static-dir*
-                                                        (conf:config :template-dir)
-                                                        (conf:config :ignore-dirs)))))
-         (files (loop :for dir :in dirs
-                      :do (ensure-directories-exist (merge-pathnames (make-relative-path dir)
-                                                                     *output-dir*))
-                      :collect (uiop:directory-files dir "*.md"))))
-    
-    (loop :for file :in files
-          :do (let* ((parsed-output (parse-markdown file))
-                     (output-path (merge-pathnames (change-extension (make-relative-path file) "html")
-                                                   *output-dir*))
-                     (templ-func (gethash (getf parsed-output :template) *templates* *default-template*)))
-                (str:to-file output-path (funcall templ-func parsed-output)
-                             :if-exists :overwrite))))
-
-  ;; now that our static files are created, lets copy over our static
-  ;;  assets if they exist
-  (when (and *static-dir* (uiop:directory-exists-p *static-dir*))
-    (copy-directory *static-dir* *output-dir*)))
+  (walk-directory-and-generate-html *root-dir*))
 
